2baacc9a9589e7d6d1cedb69cbc437a34b2958f3
John Hady <johnhady101@icloud.com>
2026-09-14 20:17:06 +0300

    adding replicaset

---
+++ replicaset/describe_replica.sh
@@ -0,0 +1,3 @@
+kubectl describe rs nginx-replicaset
+echo "Script completed"
+read -p "Press Enter to continue..."
---
+++ replicaset/replicaset.sh
@@ -0,0 +1,3 @@
+kubectl apply -f "F:\learn_kubernetes\replicaset\replicaset.yaml"
+echo "Script completed"
+read -p "Press Enter to continue..."
